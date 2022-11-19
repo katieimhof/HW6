4e0995de4a0223a5502c7c2cc6878349c2f8bb93
kimhof <kimhof@zagmail.gonzaga.edu>
2022-11-18 16:37:46 -0800

    Done

--- docs/script.js
+++ docs/script.js
@@ -10,18 +10,11 @@ $(document).ready(function () {
     var interval2;
     var interval3;
     var $bookImage = $("#books").css({
-        //position: "absolute",
-        //left: "0px",
-        //top: "0px",
         zIndex: "-1",
         minHeight: "100%",
         minWidth: "1024px",
-	
-        /* Set up proportionate scaling */
         width: "100%",
         height: "auto",
-	
-        /* Set up positioning */
         position: "fixed",
         top: "0",
         left: "0"
